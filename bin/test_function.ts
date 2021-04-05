c9306cc8c03e6000584737715b6164794c387bfa
Daopz <ingdoliveros@gmail.com>
2021-04-04 23:05:32 -0300

    More Hope

--- bin/test_function.ts
+++ bin/test_function.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-import * as cdk from '@aws-cdk/core';
+import { App } from '@aws-cdk/core';
 import { PipelineStack } from '../lib/pipeline-stack';
 
-const app = new cdk.App();
+const app = new App();
 new PipelineStack(app, 'PipelineStack');
 
 app.synth();
